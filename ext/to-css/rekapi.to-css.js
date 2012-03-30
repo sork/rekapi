22e8aa452d1f0959b538ecb33c869bd9abbd77e3
Jeremy Kahn <jeremyckahn@gmail.com>
2012-03-29 20:41:16 -0700

    Early frame serializtion attempt.

--- ext/to-css/rekapi.to-css.js
+++ ext/to-css/rekapi.to-css.js
@@ -2,8 +2,21 @@
 
   var KEYFRAME_TEMPLATE = [].join();
 
+  function serializeActorState (actor) {
+    var serializedProps = ['{'];
+    _.each(actor.get(), function (val, key) {
+      serializedProps.push(key + ': ' + val + ';');
+    });
+
+    serializedProps.push('}');
+    return serializedProps.join('');
+  };
+
   Rekapi.prototype.toCSS = function (opts) {
-    var timeline = this.exportTimeline();
+    //var timeline = this.exportTimeline();
+    var actorIds = this.getActorIds();
+    var serialized = serializeActorState(this.getActor(actorIds[0]));
+    console.log(serialized);
   };
 
 } (this.Rekapi, this.Mustache, this));
