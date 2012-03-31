9112a5c0060853e3d595e48d8dfa6f5017b1c2e1
Jeremy Kahn <jeremyckahn@gmail.com>
2012-03-31 15:27:54 -0700

    Refactored keyframe generation.

--- ext/to-css/rekapi.to-css.js
+++ ext/to-css/rekapi.to-css.js
@@ -1,22 +1,23 @@
 ;(function (Rekapi, Mustache, global) {
 
   // CONSTANTS
+  //
   var DEFAULT_GRANULARITY = 100;
 
 
   // TEMPLATES
-  var KEYFRAME_TEMPLATE = [].join();
+  //
 
-
-  function serializeActorState (actor) {
-    var serializedProps = ['{'];
-    _.each(actor.get(), function (val, key) {
-      serializedProps.push(key + ':' + val + ';');
-    });
-
-    serializedProps.push('}');
-    return serializedProps.join('');
-  };
+  /**
+   * [0]: vendor
+   * [1]: animation name
+   * [2]: keyframes
+   */
+  var KEYFRAME_TEMPLATE = [
+    '@%skeyframes %s-keyframes {'
+    ,'%s'
+    ,'}'
+  ].join('\n');
 
 
   Rekapi.prototype.toCSS = function (opts) {
@@ -30,19 +31,48 @@
 
   Rekapi.Actor.prototype.toCSS = function (opts) {
     opts = opts || {};
-    var animLength = this.kapi.animationLength();
-    var i = 0;
     var granularity = opts.granularity || DEFAULT_GRANULARITY;
+    var keyframes = generateActorKeyframes(this, granularity);
+    console.log(keyframes);
+  };
+
+
+  // UTILITY FUNCTIONS
+  //
+  function serializeActorStep (actor) {
+    var serializedProps = ['{'];
+    _.each(actor.get(), function (val, key) {
+      serializedProps.push(key + ':' + val + ';');
+    });
+
+    serializedProps.push('}');
+    return serializedProps.join('');
+  };
+
+
+  function generateActorKeyframes (actor, granularity) {
+    var animLength = actor.kapi.animationLength();
+    var i = 0;
     var serializedFrames = [];
     var percent;
 
     for (i; i <= animLength; i += animLength / granularity) {
-      this.calculatePosition(i);
+      actor.calculatePosition(i);
       percent = ((i * granularity) / animLength);
-      serializedFrames.push(percent + '% ' + serializeActorState(this));
+      serializedFrames.push(percent + '% ' + serializeActorStep(actor));
     }
 
-    console.log(serializedFrames.join('\n'));
+    return serializedFrames.join('\n');
+  }
+
+
+  var printf = Rekapi.util.printf = function (formatter, args) {
+    var composedStr = formatter;
+    _.each(args, function (arg) {
+      composedStr = composedStr.replace('%s', arg);
+    });
+
+    return composedStr;
   };
 
 } (this.Rekapi, this.Mustache, this));
